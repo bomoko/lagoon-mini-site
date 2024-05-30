7ef3b8a0a8d750ccaf6add49bae8e1fdaa438731
Blaize Kaye <blaize.kaye@amazee.io>
2024-05-31 07:34:00 +1200

    Adds lagoon and default site

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+## The following are used for the "big" version
+# for now we'll simply go ahead, download the file, and build.
+
+# # Build the manager binary
+# FROM golang:1.22-alpine as builder
+
+# WORKDIR /workspace
+# # Copy the Go Modules manifests
+# COPY siteclean/go.mod go.mod
+# # COPY siteclean/go.sum go.sum
+# # cache deps before building and copying source so that we don't need to re-download as much
+# # and so that source changes don't invalidate our downloaded layer
+# RUN go mod download
+
+# # Copy the go source
+# COPY siteclean/main.go main.go
+
+
+# # Build
+# RUN CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -a -o siteclean main.go
+
+# FROM uselagoon/nginx:24.5.0
+
+# COPY --from=builder /workspace/siteclean /siteclean
+
+# COPY siteclean/downloader/downloader.sh /downloader.sh
+
+# "little" version for demo here
+
+FROM uselagoon/nginx:latest
+ARG STATIC_FILE_LOCATION=https://github.com/bomoko/lagoon-mini-site/raw/main/default.zip
+RUN apk add wget
+ENV STATIC_FILE_LOCATION=${STATIC_FILE_LOCATION}
+
+RUN wget -O /tmp/site.zip ${STATIC_FILE_LOCATION} && unzip /tmp/site.zip /app
--- siteclean/main.go
+++ siteclean/main.go
@@ -13,54 +13,43 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(os.Args)
 	// Get the directory specified as an argument
 	rootDir := os.Args[1]
 	targetDir := os.Args[2]
 
 	// Get the absolute path of the target directory
-	absTargetDir, err := filepath.Abs(targetDir)
+	absTargetDir := filepath.Join(rootDir, targetDir)
+
+	_, err := os.Stat(absTargetDir)
 	if err != nil {
-		fmt.Printf("Error getting absolute path of %s: %v\n", targetDir, err)
-		os.Exit(1)
-	}
-	absRootDir, err := filepath.Abs(rootDir)
-	if err != nil {
-		fmt.Printf("Error getting absolute path of %s: %v\n", rootDir, err)
+		if os.IsNotExist(err) {
+			fmt.Printf("Could not find safe directory '%v'\n Did you, maybe, specify an abosulte path for the target?\n", absTargetDir)
+		} else {
+			fmt.Print(err.Error())
+		}
 		os.Exit(1)
 	}
 
-	//if true == true {
-	//	os.Exit(0)
-	//}
 	// Walk through the directory
-	err = filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			fmt.Printf("Error accessing path %q: %v\n", path, err)
-			return err
-		}
-
-		absPath, err := filepath.Abs(path)
-		if err != nil {
-			fmt.Printf("Error getting absolute path of %s: %v\n", path, err)
-			os.Exit(1)
-		}
-
-		fmt.Printf("path: %v \n absTargetDir: %v \n absRootDir: %v\n\n", absPath, absTargetDir, absRootDir)
-		// Check if it's a directory (excluding the target directory)
-		if info.IsDir() && absPath != absTargetDir && absPath != absRootDir {
-			fmt.Printf("Deleting directory: %s\n", absPath)
-			err := os.RemoveAll(absPath)
-			if err != nil {
-				fmt.Printf("Error deleting directory %s: %v\n", absPath, err)
-			}
-		}
-		return nil
-	})
+	dirEntries, err := os.ReadDir(rootDir)
 
 	if err != nil {
 		fmt.Printf("Error walking directory: %v\n", err)
 		os.Exit(1)
+	}
+
+	for _, e := range dirEntries {
+
+		fullPath := filepath.Join(rootDir, e.Name())
+		if e.IsDir() && fullPath != absTargetDir {
+			fmt.Printf("Found dir to delete '%v' \n", fullPath)
+			err := os.RemoveAll(fullPath)
+			if err != nil {
+				fmt.Printf("Unable to delete directory '%v' - failing\n", fullPath)
+				os.Exit(1)
+			}
+		}
+
 	}
 
 	fmt.Println("Cleanup complete!")
